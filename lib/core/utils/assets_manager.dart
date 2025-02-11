a29e9648951b11605cbce89394b0f1e95759a276
eslammahmoud <solom6112002@gmail.com>
2025-02-12 00:07:11 +0200

    Finish splash screen. Add animate_do package. Add assets folder.

--- lib/core/utils/assets_manager.dart
+++ lib/core/utils/assets_manager.dart
@@ -1,25 +1,11 @@
-const String imagePath="assets/images";
-const String iconPath="assets/icons";
+const String imagePath="assets/image";
+const String iconPath="assets/icon";
 
 abstract class ImageAssets{
- static const String logoBuy="$imagePath/logoBuy.jpg";
- static const String splashScreen="$imagePath/Splash Screen.png";
- static const String imageAdv1="$imagePath/imageAdvOne.png";
- static const String imageAdv2="$imagePath/imageAdvTwo.png";
- static const String imageAdv3="$imagePath/imageAdvThree.png";
+static const String splashBackground="$imagePath/splash_background_image.png";
 }
 abstract class IconAssets{
-static const String iconHome="$iconPath/icon_home.png";
-static const String iconCategories="$iconPath/icon_categories.png";
-static const String iconProfile="$iconPath/icon_profile.png";
-static const String iconFavorite="$iconPath/icon_favorite.png";
-static const String iconStar="$iconPath/icon_star.png";
-static const String iconCategoryFavorite="$iconPath/icon_category_favorite.png";
-static const String iconSelectedFavorite="$iconPath/icon_selected_favorite.png";
-static const String iconSubtractCircle="$iconPath/iconSubtractCircle.png";
-static const  String iconPlusCircle="$iconPath/iconPlusCircle.png";
-static const String iconSuccess="$iconPath/face_success_icon.json";
-static const String iconWrong="$iconPath/face_wrong_icon.json";
+static const String movieIcon="$iconPath/movie_icon.png";
 }
 abstract class SvgImages{
 
--- lib/presentation/splash/splash_screen.dart
+++ lib/presentation/splash/splash_screen.dart
@@ -1,10 +1,46 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:movie_app/core/routes_manager/page_routes.dart';
+import 'package:movie_app/core/utils/assets_manager.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
   @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    Future.delayed(
+      const Duration(milliseconds: 2000),
+      () {
+        Navigator.pushNamed(context, PagesRoutes.loginScreen);
+      },
+    );
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        body: Container(
+      height: double.infinity,
+      width: double.infinity,
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+            image: AssetImage(ImageAssets.splashBackground), fit: BoxFit.fill),
+      ),
+      child: FadeInDown(
+        duration: const Duration(milliseconds: 1500),
+        from: 500.h,
+        child: Image.asset(
+          IconAssets.movieIcon,
+          scale: 1,
+        ),
+      ),
+    ));
   }
 }
